b2c3b22628dc9940d6c946f5bc34eb2c0a2a7aca
Stuart Matamoros <smatamoros@number8.com>
2020-07-11 22:51:57 -0600

    Adding the Solution

---
+++ DesignPatterns/SOLID/Program.cs
@@ -0,0 +1,19 @@
+﻿using SOLID.Principles;
+using System;
+
+namespace SOLID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSingleResponsibility();
+        }
+
+        private static void TestSingleResponsibility()
+        {
+            SingleResponsibility principle = new SingleResponsibility();
+            principle.Test();
+        }
+    }
+}
