c9f08268677fec7461cef8baf943ea1049d4364a
Stuart Matamoros <smatamoros@number8.com>
2020-07-11 23:29:15 -0600

    Added Single Responsibility Principle

--- DesignPatterns/SOLID/Principles/SingleResponsibility.cs
+++ DesignPatterns/SOLID/Principles/SingleResponsibility.cs
@@ -1,14 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace SOLID.Principles
 {
     public class SingleResponsibility
     {
+        private const bool OVERWRITE = true;
         public void Test()
         {
+            var myJournal = new Journal();
+            myJournal.AddEntry("I ate an apple today");
+            myJournal.AddEntry("I rode my bike today");
+            System.Console.WriteLine(myJournal);
 
+            string savedFilePath = JournalPersistence.SaveToFile(myJournal, "happyDay.txt", OVERWRITE);
+            System.Console.WriteLine($"Saved Journal to: {savedFilePath}");
+        }
+    }
+
+    class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+
+            return count;   // memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    static class JournalPersistence
+    {
+        public static string SaveToFile(Journal journal, string fileName, bool overwrite = false)
+        {
+            string appFolderPath = AppDomain.CurrentDomain.BaseDirectory;
+            string journalFolderPath = Path.Combine(appFolderPath, "JournalFiles");
+            Directory.CreateDirectory(journalFolderPath);
+            string journalFilePath = Path.Combine(journalFolderPath, fileName);
+
+            bool fileIsNew = !File.Exists(journalFilePath);
+            if(overwrite || fileIsNew)
+            {
+                File.WriteAllText(journalFilePath, journal.ToString());
+            }
+
+            return journalFilePath;
         }
     }
 }
--- DesignPatterns/SOLID/Program.cs
+++ DesignPatterns/SOLID/Program.cs
@@ -8,11 +8,20 @@ namespace SOLID
         static void Main(string[] args)
         {
             TestSingleResponsibility();
+
+            System.Console.WriteLine("Press any key to finish...");
+            System.Console.ReadLine();
         }
 
         private static void TestSingleResponsibility()
         {
             SingleResponsibility principle = new SingleResponsibility();
+            principle.Test();
+        }
+
+        private static void TestOpenClose()
+        {
+            OpenClose principle = new OpenClose();
             principle.Test();
         }
     }
